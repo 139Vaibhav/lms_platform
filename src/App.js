5e614e0deeb770b150b55806afed4fb012c8b0ee
139Vaibhav <vaibhavkumarsingh139@gmail.com>
2020-08-11 20:11:55 +0530

    SideNav[WIP]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      
+      <div id="highlighted_area">
+        <div id="Frame_1">
+          <div id="highlighted_area_copy_44_1"></div>
+          <div id="highlighted_area_copy_44_2"></div>
+          <div id="highlighted_area_copy_44_3"></div>
+          <div id="highlighted_area_copy_44_4"></div>
+        </div>
+        
+        <div id="user_copy_18">
+        <img id="user_copy_18_path" src={require("./Dashboard/user_copy_18.png")}></img>
+          <img id="mask" src={require("./Dashboard/Mask.png")}></img>
+        </div>
+        
+      </div>
     </div>
   );
 }
